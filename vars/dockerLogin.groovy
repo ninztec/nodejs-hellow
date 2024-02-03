ac589afa4d7d378de19b8c07ead847ddfafd194b
ninztec <152024442+ninztec@users.noreply.github.com>
2024-02-03 15:58:37 +0530

    Update dockerLogin.groovy

--- vars/dockerLogin.groovy
+++ vars/dockerLogin.groovy
@@ -1,1 +1,10 @@
+// vars/dockerLogin.groovy
+
+def call(String credentialsId) {
+    withCredentials([string(credentialsId: credentialsId, variable: 'PASSWORD')]) {
+        script {
+            sh 'docker login -u rjshk013 -p $PASSWORD'
+        }
+    }
+}
 
